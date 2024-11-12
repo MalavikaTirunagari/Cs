bab7d86116d62d6cfb229c36597adc9261e8a264
Malavika <malavikatirunagari07@gmail.com>
2024-11-12 11:04:15 +0530

    b

--- Cs.java
+++ Cs.java
@@ -1,5 +1,5 @@
 class Cs{
-    public static void main(string []args){
+    public static void main(String []args){
         System.out.println("hiiiiiiii");
         System.out.println("byeee");
         System.out.println("tata");
